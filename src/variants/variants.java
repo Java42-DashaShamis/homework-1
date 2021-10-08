9b2157ec867840e4c20f5843f4e48ec201b3657a
korsa <korsa@Dellchik>
2021-10-08 18:25:41 +0300

    adds variant 3

--- src/variants/variants.java
+++ src/variants/variants.java
@@ -3,5 +3,6 @@ package variants;
 public class variants {
 	public static void main(String[] args) {
 		System.out.println("There are following variants:");
+		System.out.println("Variant 3");
 	}
 }
